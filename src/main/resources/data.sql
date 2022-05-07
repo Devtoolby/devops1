11a1dbb3f53e3b7cbb5495b3582f1e741528f4d1
Devtoolby <104337792+Devtoolby@users.noreply.github.com>
2022-05-08 04:38:36 +0800

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://www.delihemp-pro.fr/847-medium_default/odeur-interdite-og-kush.jpg', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://www.retroplus.ma/pub/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/k/i/kids_s_panton_orange_1.jpg', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://www.aunainbleu.com/221-home_default/cheval-a-bascule.jpg', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://www.houstalet.com/9635-medium_default/coq-3d-en-bois.jpg', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_2000/img/flacon-en-verre-noir-h52-1000-12-4-220502_1.jpg', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://ae01.alicdn.com/kf/H3b2cf333622240b09273693c01ffdcd59/Horloge-murale-suspension-pour-les-enseignants-en-biochimie-tableau-p-riodique-de-l-heure-chimique-article.jpg_Q90.jpg_.webp', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://img.edilportale.com/product-thumbs/b_MILL-Round-table-Karl-Andersson-S%C3%B6ner-80641-rela7fe69ff.jpg', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://i.pinimg.com/564x/0e/50/52/0e50523a6ec92cdbe0c327d4506ab01c.jpg', 730.0);
